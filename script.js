32cf12fc86730cdf190c14e75b3201b921f07ed1
Rolando2 <rolando.far9@gmail.com>
2025-01-30 12:59:00 -0600

    Added touch activation

---
+++ script.js
@@ -0,0 +1,20 @@
+const letter = document.getElementById("letterContainer");
+
+function openLetter() {
+    const lidShadow = document.getElementById("lidShadow");
+    const lid = document.getElementById("letterLidContainer");
+    const glowEffect1 = document.getElementById("leftShadow")
+    const glowEffect2 = document.getElementById("rightShadow")
+    const glowEffect3 = document.getElementById("bottomShadow")
+
+    lid.style.animation = "openLetter 1.5s forwards ease-out";
+    lid.style.animationDelay = "200ms";
+    lidShadow.style.animation = "noShadow 1.5s forwards ease-out";
+    lidShadow.style.animationDelay = "200ms";
+    glowEffect1.style.animation = "pulse 1s ease-in-out alternate infinite";
+    glowEffect1.style.animationDelay = "2s";
+    glowEffect2.style.animation = "pulse 1s ease-in-out alternate infinite";
+    glowEffect2.style.animationDelay = "2s";
+    glowEffect3.style.animation = "pulse 1s ease-in-out alternate infinite";
+    glowEffect3.style.animationDelay = "2s";
+}
