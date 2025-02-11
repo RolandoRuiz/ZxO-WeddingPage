6118295c73ec9c09201c3cf33628c001b6256948
Rolando Ruiz <rolando.far9@gmail.com>
2025-02-11 00:59:14 -0600

    Removed branch, focused on adding seal animation and index

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function openLetter() {
 
     const letter = document.getElementById("letterContainer");
-    const lidShadow = document.getElementById("lidWrapper");
+    const lidShadow = document.getElementById("openingWrapper");
     const lid = document.getElementById("lidContainer");
+    const seal = document.getElementById("seal")
+    const seal2 = document.getElementById("lidSealContainer");
     const glowEffect1 = document.getElementById("leftShadow")
     const glowEffect2 = document.getElementById("rightShadow")
     const glowEffect3 = document.getElementById("bottomShadow")
 
+    seal2.style.animation = "indexSeal 1.5s forwards ease-out";
+    seal2.style.animationDelay = "200ms";
+    seal.style.animation = "rotateSeal 1.5s forwards ease-out";
+    seal.style.animationDelay = "200ms";
     lid.style.animation = "openLetter 1.5s forwards ease-out";
     lid.style.animationDelay = "200ms";
     lidShadow.style.animation = "fadeLidShadow 1.5s forwards ease-out";
