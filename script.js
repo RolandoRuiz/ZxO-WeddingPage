d6c764514d669790651db1634bff13e8df09946e
Rolando2 <rolando.far9@gmail.com>
2025-02-11 18:58:50 -0600

    Added rotation to the seal

--- script.js
+++ script.js
@@ -9,14 +9,16 @@ function openLetter() {
     const glowEffect2 = document.getElementById("rightShadow")
     const glowEffect3 = document.getElementById("bottomShadow")
 
+    lid.style.animation = "openLetter 1.5s forwards ease-out";
+    lid.style.animationDelay = "200ms";
+    
+    lidShadow.style.animation = "fadeLidShadow 1.5s forwards ease-out";
+    lidShadow.style.animationDelay = "200ms";
+
     seal2.style.animation = "indexSeal 1.5s forwards ease-out";
     seal2.style.animationDelay = "200ms";
     seal.style.animation = "rotateSeal 1.5s forwards ease-out";
     seal.style.animationDelay = "200ms";
-    lid.style.animation = "openLetter 1.5s forwards ease-out";
-    lid.style.animationDelay = "200ms";
-    lidShadow.style.animation = "fadeLidShadow 1.5s forwards ease-out";
-    lidShadow.style.animationDelay = "200ms";
     /*glowEffect1.style.animation = "pulse 1s ease-in-out alternate infinite";
     glowEffect1.style.animationDelay = "2s";
     glowEffect2.style.animation = "pulse 1s ease-in-out alternate infinite";
