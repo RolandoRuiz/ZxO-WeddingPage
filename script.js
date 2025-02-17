60a8e518f66b6d7d408ddd3eaaf87a836fc59294
Rolando Ruiz <rolando.far9@gmail.com>
2025-02-16 23:19:22 -0600

    Added background and removed seal animation

--- script.js
+++ script.js
@@ -13,17 +13,17 @@ function openLetter() {
     const glowEffect2 = document.getElementById("rightShadow")
     const glowEffect3 = document.getElementById("bottomShadow")
 
-    lid.style.animation = "openLetter 5s forwards ease-out";
+    lid.style.animation = "openLetter 5s forwards linear";
 
-    lidShadow.style.animation = "fadeLidShadow 5s forwards ease-out";
+    lidShadow.style.animation = "fadeLidShadow 5s forwards linear";
 
-    seal.style.animation = "sealOpening 5s forwards ease-out";
+    /*seal.style.animation = "sealOpening 5s forwards linear";
 
-    middleSeal.style.animation = "showMiddle 5s forwards ease-out";
+    middleSeal.style.animation = "showMiddle 5s forwards linear";
     
-    backSeal.style.animation = "rotateSeal1 5s forwards ease-out,  moveSeal 5s forwards ease-out";
+    backSeal.style.animation = "rotateSeal1 5s forwards linear,  moveSeal 5s forwards linear";
 
-    frontSeal.style.animation = "rotateSeal2 5S forwards ease-out";
+    frontSeal.style.animation = "rotateSeal2 5s forwards linear";
 
 
     /*glowEffect1.style.animation = "pulse 1s ease-in-out alternate infinite";
